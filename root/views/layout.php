7bcb8162928f218c14c03c80cb575b1f9b0453dd
kouheyHEY <vudtarm@gmail.com>
2023-04-17 18:43:22 +0900

    update rule

--- root/views/layout.php
+++ root/views/layout.php
@@ -9,7 +9,7 @@
     <form action="./index.php" method="get">
         <ul class="product-navbar fadeDown">
             <?php foreach (Config::getMasterData("CONTENTS") as $content): ?>
-                <?php $selectContent = ($content == $_SESSION["CONTENT"]) ? '-selected' : '' ?>
+                <?php $selectContent = ($content == "ABOUT") ? '-selected' : '' ?>
                 <li class="button-link-linear<?= $selectContent ?>">
                     <button type="submit" name="CONTENT" value="<?= $content ?>" id="<?= $content ?>"><?= $content ?></button>
                 </li>
