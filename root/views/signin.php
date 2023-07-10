24a4e5098a226711acf57da89041ecbd40814e8e
kouHEY <51882041+kouheyHEY@users.noreply.github.com>
2023-07-11 08:45:56 +0900

    signin.php の更新

--- root/views/signin.php
+++ root/views/signin.php
@@ -3,16 +3,24 @@
 
     <div class="fadeDown_2">
 
-        <!-- コンテンツタイトル -->
-        <div class="content-title">
-            <span>
-                <?= $contentTitle ?>
-            </span>
-        </div>
-
         <!-- サインインフォーム -->
-
         <form method="POST" name="signinForm" action="/signin">
+
+            <!-- ユーザ名入力欄 --> 
+             <div class="user-name"> 
+                 <div class="content-title"> 
+                     <span>UserName</span> 
+                 </div> 
+                 <input id="" name="user-name" type="text" placeholder="ユーザ名" <?php if (isset($_SESSION['user-name'])) : ?> value="<?= $_SESSION['user-name'] ?>" <?php endif; ?> /> 
+             </div>
+
+            <!-- パスワード入力欄 --> 
+             <div class="password"> 
+                 <div class="content-title"> 
+                     <span>Password</span> 
+                 </div> 
+                 <input id="" name="password" type="password" /> 
+             </div>
 
         </form>
 
