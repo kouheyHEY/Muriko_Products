e145a61cd7cdd0e9730efa0aa50921d682acefdc
kouHEY <51882041+kouheyHEY@users.noreply.github.com>
2023-07-11 12:55:41 +0900

    signin.php の更新

--- root/views/signin.php
+++ root/views/signin.php
@@ -7,21 +7,30 @@
         <form method="POST" name="signinForm" action="/signin">
 
             <!-- ユーザ名入力欄 --> 
-             <div class="user-name"> 
-                 <div class="content-title"> 
-                     <span>UserName</span> 
-                 </div> 
-                 <input id="" name="user-name" type="text" placeholder="ユーザ名" <?php if (isset($_SESSION['user-name'])) : ?> value="<?= $_SESSION['user-name'] ?>" <?php endif; ?> /> 
-             </div>
+            <div class="user-name"> 
+                <div class="content-title"> 
+                    <span>UserName</span> 
+                </div> 
+                <input id="" name="user-name" type="text" placeholder="ユーザ名" <?php if (isset($_SESSION['user-name'])) : ?> value="<?= $_SESSION['user-name'] ?>" <?php endif; ?> /> 
+            </div>
 
             <!-- パスワード入力欄 --> 
-             <div class="password"> 
-                 <div class="content-title"> 
-                     <span>Password</span> 
-                 </div> 
-                 <input id="" name="password" type="password" /> 
-             </div>
+            <div class="password"> 
+                <div class="content-title"> 
+                    <span>Password</span> 
+                </div> 
+                <input id="" name="password" type="password" /> 
+            </div>
 
+            <!-- 各種ボタン --> 
+            <ul id="signin-button"> 
+                <li class="button-link-linear"> 
+                    <a href="javascript:signinForm.submit()">sign in</a> 
+                </li> 
+                <li class="button-link-linear"> 
+                    <button onclick="clearInput();">clear</button> 
+                </li> 
+            </ul>
         </form>
 
     </div>
